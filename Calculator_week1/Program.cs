b4207c511bdf13ce0776dbf5807a69c325eecea4
Björn Elfvin <bjorn@elfvin.se>
2021-02-19 10:50:00 +0100

    Update version with new and looping methods for addition and subtraction.

--- Calculator_week1/Program.cs
+++ Calculator_week1/Program.cs
@@ -6,24 +6,23 @@ namespace Calculator_week1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello dear user, I'm here so help you with some calculus..\nWhat would you like to calculate?\nI can handle addition, subtraction, multiplying and division.");
-            Console.Write("First, tell me what calculation method you want to use.  A for addition, S for subtraction, M for multiplying and D for division. Fire away! ");
+            Console.WriteLine("Hello dear user, I'm here so help you with some calculus..\nWhat would you like to calculate?\nI can handle addition, subtraction, multiplying and division.\n First, tell me what calculation method you want to use.\n A for addition, S for subtraction, M for multiplying and D for division. Fire away! ");
 
             bool keepAlive = true;
             while (keepAlive)
             {
                 try
                 {
-                    char assigmentChoice = char.Parse(Console.ReadLine().ToUpper() ?? "No, type in one character (A,S,M,D) and hitreturn for calculus method.");
-
-                    //Use appropritate method call....
+                    char assigmentChoice = char.Parse(Console.ReadLine().ToUpper() ?? "No, type in one character (A,S,M,D) and hit return to select calculus method.");
                     switch (assigmentChoice)
                     {
                         case 'A':
-                            Addition();
+                            //Addition();
+                            AdditionLoop();
                             break;
                         case 'S':
-                            Subtraction();
+                            //Subtraction();
+                            SubtractionLoop();
                             break;
                         case 'M':
                             Multiplying();
@@ -42,10 +41,10 @@ namespace Calculator_week1
                 }
                 catch
                 {
-                    Console.WriteLine("Pls, type in one character (A,S,M,D) for calculus method and hitreturn.");
+                    Console.WriteLine("Pls, type in one character (A,S,M,D) for calculus method and hit return.");
                 }
             }
-            static double AskUserForInput(string what )
+            static double AskUserForInput(string what = "a number")
             {
                 Console.Write($"Ok, hit me with {what}..  ");
                 double ret = Double.Parse(Console.ReadLine());
@@ -54,22 +53,50 @@ namespace Calculator_week1
             static void Addition()
             {
                 Console.WriteLine("Addition is fun, lets do it! ");
-                double userInput1 = AskUserForInput("a number");
+                double userInput1 = AskUserForInput();
                 double userInput2 = AskUserForInput("another number");
                 double sum = userInput1 + userInput2;
                 Console.WriteLine($"Sum is: {sum}");
             }
+            static void AdditionLoop()
+            {
+                Console.WriteLine("Addition is fun, lets do it! ");
+                double sum = AskUserForInput();
+
+                bool loopAlive = true;
+                while (loopAlive)
+                {
+                    double userInput2 = AskUserForInput("another number");
+                    sum = sum + userInput2;
+                    Console.WriteLine($"Sum is (now): {sum}. Give me a letter to quit.");
+                }
+            }
+
             static void Subtraction()
             {
-                Console.WriteLine("Sutraction is lots of fun! ");
-                double userInput1 = AskUserForInput("a number");
+                Console.WriteLine("Subtraction is lots of fun! ");
+                double userInput1 = AskUserForInput();
                 double userInput2 = AskUserForInput("a number to subtract from the first number");
                 double sum = userInput1 - userInput2;
                 Console.WriteLine($"Sum is: {sum}");
             }
+            static void SubtractionLoop()
+            {
+                Console.WriteLine("Subtraction is lots of fun! ");
+                double sum = AskUserForInput();
+
+                bool loopAlive = true;
+                while (loopAlive)
+                {
+                    double userInput2 = Math.Abs(AskUserForInput("Another number. It is going to be subtracted to the sum disregarding if it's a positive or negative number. You have been warned.... ;) "));
+                    sum = sum - userInput2;
+                    Console.WriteLine($"Sum is (now): {sum}. Give me a letter to quit.");
+                }
+            }
+
             static void Multiplying()
             {
-                double userInput1 = AskUserForInput("a number");
+                double userInput1 = AskUserForInput();
                 double userInput2 = AskUserForInput("a number to multiply with the first number");
                 double sum = userInput1 * userInput2;
                 Console.WriteLine($"Product is: {sum}");
@@ -89,6 +116,5 @@ namespace Calculator_week1
                 }
             }
         }
-
     }
 }
